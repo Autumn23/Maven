2e58a730b9a2e7737d13d57761aac404882f45ad
Git Server Admin <admin@git-server>
2020-05-15 13:37:13 +0000

    Add initial repository

---
+++ src/main/java/javaApp/EvenSum.java
@@ -0,0 +1,16 @@
+package javaApp;
+
+public class EvenSum {
+
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int even[] = new int[num];
+        int sum = 0,j = 0;
+        String evennums = "";
+        //Insert your code here
+
+
+        System.out.println(evennums);
+        System.out.println(sum);
+    }
+}  
